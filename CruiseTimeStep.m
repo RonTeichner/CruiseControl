887649aaf71d82e0da9c6905ad37eac95d396c89
Ron Teichner <ron.teichner@campus.technion.ac.il>
2019-04-06 21:51:52 +0300

    an update

--- CruiseTimeStep.m
+++ CruiseTimeStep.m
@@ -1,4 +1,4 @@
-function [sStateVec,u] = CruiseTimeStep(sStateVec, vRef, sParams, theta, gear)
+function [sStateVec,u] = CruiseTimeStep(sStateVec, vRef, sModelParams,sSimParams, theta, gear)
 % Inputs:
 % theta - road angle [rad]
 % vRef - reference speed [m/s]
@@ -7,27 +7,27 @@ function [sStateVec,u] = CruiseTimeStep(sStateVec, vRef, sParams, theta, gear)
 %
 % Ron Teichner, 05.04.2019
 
-
+ts = 1/sSimParams.fs;
 % measurement noise:
-e_k = sParams.std_e * randn; % [m/s]
+e_k = sModelParams.std_e * randn; % [m/s]
 
 % control:
-z_k1 = vRef - sStateVec.v - e_k;
-u = Kp*z_k1 + Ki*sStateVec.z;
+u_k = Kp*(vRef - sStateVec.v - e_k) + Ki*sStateVec.z;
+z_k1 = sStateVec.z + (vRef - sStateVec.v - e_k)*ts;
 
 % torque:
-T = sParams.Tm*(1 - sParams.beta*(sParams.alpha_n(gear)*sStateVec.v/sParams.omega_m - 1)^2);
+T = sModelParams.Tm*(1 - sModelParams.beta*(sModelParams.alpha_n(gear)*sStateVec.v/sModelParams.omega_m - 1)^2);
 
 % wind noise:
-b_k = sParams.std_b * randn; % [m/s]
+b_k = sModelParams.std_b * randn; % [m/s]
 
 % forces:
-Fg = sParams.m * sParams.g * sin(theta);
-Fr = sParams.m * sParams.g * sParams.Cr * sStateVec.v;
-Fa = 0.5 * sParams.rho * sParams.Cd * sParams.A * sStateVec.v^2;
+Fg = sModelParams.m * sModelParams.g * sin(theta);
+Fr = sModelParams.m * sModelParams.g * sModelParams.Cr * sStateVec.v;
+Fa = 0.5 * sModelParams.rho * sModelParams.Cd * sModelParams.A * sStateVec.v^2;
 Fd = Fg + Fr + Fa;
 
-v_k1 = (sParams.alpha_n(gear) * u * T - Fd)/m + b_k;
+v_k1 = sStateVec.v + (sModelParams.alpha_n(gear) * u_k * T - Fd)/m*ts + b_k*ts;
 
 sStateVec.v = v_k1;
 sStateVec.z = z_k1;
